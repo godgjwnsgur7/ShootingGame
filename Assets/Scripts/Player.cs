68c2c64e2ca93c9cd29ca4b60389db2a8f817322
HeoJoonHyuk <67333305+godgjwnsgur7@users.noreply.github.com>
2022-02-04 19:24:45 +0900

    Commit 2
    2. 총알 발사 구현하기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,36 +4,97 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    int bulletSpeed;
+
     public float speed;
+    public float power;
+    public float maxPower;
+    public float maxShotDelay;
+    public float curShotDelay;
 
     public bool isTouchTop;
     public bool isTouchBottom;
     public bool isTouchLeft;
     public bool isTouchRight;
 
+    public GameObject bulletObjA;
+    public GameObject bulletObjB;
+
     Animator anim;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
+        bulletSpeed = 7;
         speed = 5;
+        power = 1;
+        maxPower = 3;
+        maxShotDelay = 0.15f;
     }
 
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal"); // 수평(x축)
+        Move();
+        Fire();
+        Reload();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
         if ((isTouchRight && h == 1) || (isTouchLeft && h == -1)) h = 0;
 
-        float v = Input.GetAxisRaw("Vertical"); // 수직 (y축)
+        float v = Input.GetAxisRaw("Vertical");
         if ((isTouchTop && v == 1) || (isTouchBottom && v == -1)) v = 0;
 
-        Vector3 curPos = transform.position; // 현재 위치 가져옴
-        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime; // 이동할 속도
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
 
-        transform.position = curPos + nextPos; // 이동
+        transform.position = curPos + nextPos;
 
         if (Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal"))
-            anim.SetInteger("Input", (int)h); // 강제 형 변환
+            anim.SetInteger("Input", (int)h);
+    }
+
+    void Fire()
+    {
+        if (!Input.GetButton("Fire1") || curShotDelay < maxShotDelay)
+            return;
+
+        switch(power)
+        {
+            case 1: // Power 1
+                GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation);
+                Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+                rigid.AddForce(Vector2.up * bulletSpeed, ForceMode2D.Impulse);
+                break;
+            case 2: // Power 2
+                GameObject bulletR = Instantiate(bulletObjA, transform.position + Vector3.right*0.1f, transform.rotation);
+                GameObject bulletL = Instantiate(bulletObjA, transform.position + Vector3.left*0.1f, transform.rotation);
+                Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                rigidR.AddForce(Vector2.up * bulletSpeed, ForceMode2D.Impulse);
+                rigidL.AddForce(Vector2.up * bulletSpeed, ForceMode2D.Impulse);
+                break;
+            case 3: // Power 3
+                GameObject bulletRR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.35f, transform.rotation);
+                GameObject bulletCC = Instantiate(bulletObjB, transform.position, transform.rotation);
+                GameObject bulletLL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.35f, transform.rotation);
+                Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidCC = bulletCC.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
+                rigidRR.AddForce(Vector2.up * bulletSpeed, ForceMode2D.Impulse);
+                rigidCC.AddForce(Vector2.up * bulletSpeed, ForceMode2D.Impulse);
+                rigidLL.AddForce(Vector2.up * bulletSpeed, ForceMode2D.Impulse);
+                break;
+        }
+
+        curShotDelay = 0;
+    }
+
+    void Reload()
+    {
+        curShotDelay += Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
