8a627634b5ff45bfb6baa939c455e4ac9fe56bfc
HeoJoonHyuk <67333305+godgjwnsgur7@users.noreply.github.com>
2022-02-04 20:34:30 +0900

    Commit 3
    3. 적 비행기 만들기

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public int health;
+    public Sprite[] sprites; // 기본상태 0, 피격상태 1
+
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.down * speed;
+    }
+    void OnHit(int dmg)
+    {
+        health -= dmg;
+        spriteRenderer.sprite = sprites[1]; // 피격 이미지
+        Invoke("ReturnSprite", 0.1f);
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void ReturnSprite()
+    {
+        spriteRenderer.sprite = sprites[0];
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "BorderBullet")
+            Destroy(gameObject);
+        else if (collision.gameObject.tag == "PlayerBullet")
+        {
+            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+            OnHit(bullet.dmg);
+            Destroy(collision.gameObject); // 닿은 총알 파괴
+        }
+    }
+}
